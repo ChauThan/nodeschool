71a51befbc69b71b82547dc0041ff5904dcc8997
chauthan <simplesoul.chau@gmail.com>
2015-12-21 13:44:37 +0000

    add more exercise

--- functional-javascript-workshop/reduce.js
+++ functional-javascript-workshop/reduce.js
@@ -1,5 +1,14 @@
 function countWords(inputWords){
-  
+  return inputWords.reduce(function(obj, curr){
+        if(!obj[curr]){
+            obj[curr] = 1;
+        }
+        else{
+            obj[curr] = obj[curr] + 1;
+        }
+        
+        return obj;
+  }, {});
 }
 
 module.exports = countWords;
